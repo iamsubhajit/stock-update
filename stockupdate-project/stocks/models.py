1918a2903a2788292add06eeddc6a1548786de8b
Subhajit Maity <smy5@3ds.com>
2020-01-14 15:39:27 +0530

    Added static css and js files

--- stockupdate-project/stocks/models.py
+++ stockupdate-project/stocks/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Stock(models.Model):
     image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=50)
     summary = models.CharField(max_length=200)
     def __str__(self):
         return self.summary
--- stockupdate-project/stocks/views.py
+++ stockupdate-project/stocks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-#from .models import 
+from .models import Stock
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'stocks/home.html')
+    stocks = Stock.objects
+    return render(request, 'stocks/home.html', {'stocks':stocks})
